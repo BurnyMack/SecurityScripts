4ac7a58299c0ea9cfba0b1cc2c273b918685ae49
Ben Mackereth <ben.mackereth@hotmail.co.uk>
2023-08-29 07:06:35 +0100

    updated packages import

--- TSS_Python/setup.py
+++ TSS_Python/setup.py
@@ -4,6 +4,6 @@ setup(
     name='TSS_Python',
     author = 'Ben Mackereth',
     version='0.1',
-    packages=find_packages(include=['Modules/TSS_Python']),
+    packages=find_packages(include=['TSS_Python']),
     install_requires=['requests']
 )
